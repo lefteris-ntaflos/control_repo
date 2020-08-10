869c9f72c85b699bd98a8c24cf93872353dae1e6
lefteris-ntaflos <lefteris-ntaflos@users.noreply.github.com>
2020-08-10 15:05:50 +0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDNBvat1tewUXEDI20+wMDF0dcJ8kDOyxGZOBuY8fQ+EuxkV/N5NMIY5kqH0CFCWLUhCYMU6IvEhB6erSDpFGNt6UtU/w4CkG9xHfcowqeYpKOmnzHDjK5+7ZowPEFUuk6kYfQtdL37r15vfB/yO3xWmal7HSYMeUAiDmaN5uXxrWiCcu6AilQ9nD1K9xSIdgy2rMXj7+9wfoj6Z+ouFkN5gS5DQ6YISmQGzxFktGG7HIrgZVyXMTwxJfjMvS/DxCONUD0gnkacD7ucVskCoSt2iNubr5xsP+xGvoNiqtWx2Mkdyu79WClGYfX6ovVEaIK0cVzNM/55smFvZPlmg3rn',
 	}  
 }
